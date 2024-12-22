5ebd6ad961a20c0e260ddb57af620fb433f45882
Athul Krishna <akkv.2507@gmail.com>
2024-12-22 15:36:35 +0530

    Time: 0 ms (100%), Space: 4.8 MB (90.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.go
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.go
@@ -1,12 +1,9 @@
 func reverseWords(s string) string {
-    result:=[]string{}
-    split:=strings.Split(s," ")
-    for i:=len(split)-1;i>=0;i--{
-        if split[i]!=""{
-            result=append(result,split[i])
-        }
-    }
-
-    return strings.Join(result," ")
+    split:=strings.Fields(s)
+	for i,j:=0,len(split)-1;i<j;i,j=i+1,j-1{
+	  split[i],split[j]=split[j],split[i]
+	}
+    
+    return strings.Join(split," ")
      
 }
