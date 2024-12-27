fe2f29b6f66e0379ee26cd4b80ac94d1202ad6f4
Athul Krishna <akkv.2507@gmail.com>
2024-12-27 10:32:14 +0530

    Time: 0 ms (100%), Space: 4.8 MB (84.55%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.go
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.go
@@ -1,10 +1,5 @@
 func findMin(nums []int) int {
-    min:=math.MaxInt
-    for _, num:= range nums{
-        if num<min{
-            min=num
-        }
-    }
-    return min
+   sort.Ints(nums)
+   return nums[0]
 
 }
