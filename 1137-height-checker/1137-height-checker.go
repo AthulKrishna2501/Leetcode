280aedc64fa1e597970d6d99e2bcdd7d40d829d5
Athul Krishna <akkv.2507@gmail.com>
2025-01-18 09:40:27 +0530

    Time: 0 ms (100%), Space: 4.1 MB (81.82%) - LeetHub

--- 1137-height-checker/1137-height-checker.go
+++ 1137-height-checker/1137-height-checker.go
@@ -3,16 +3,14 @@ func heightChecker(heights []int) int {
     expected=append(expected,heights...)
 
     sort.Ints(expected)
-    result:=[]int{}
+    count:=0
+
     for i:=range heights{
         if heights[i]!=expected[i]{
-            result=append(result,i)
+           count++
         }
     }
-    count:=0
-   for i:=0;i<len(result);i++{
-    count++
-   }
 
     return count
+   
 }
